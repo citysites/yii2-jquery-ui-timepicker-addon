bf1da2a74e94d67a108d0251149d7d9bcdf886fc
nikosid <nikosid@gmail.com>
2019-10-04 12:15:58 +0200

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Yii2 wrap for https://github.com/osano/cookieconsent
+
+## Documentation
+
+All documantation about cookieconsent you can find [here](https://cookieconsent.osano.com/documentation/about-cookie-consent/)
+
+## Installation
+
+### Composer
+
+The preferred way to install this extension is through [Composer](http://getcomposer.org/).
+
+Either run ```composer require citysites/yii2-cookieconsent```
+
+or add ```"citysites/yii2-cookieconsent": "*"``` to the require section of your ```composer.json```
+
+## Usage
+
+```php
+use \citysites\widgets\CookieConsent;
+...
+echo CookieConsent::widget();
+```
---
+++ assets/JuiTimepickerAddonAsset.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace citysites\assets;
+
+use yii\jui\JuiAsset;
+use yii\web\AssetBundle;
+
+class JuiTimepickerAddonAsset extends AssetBundle
+{
+    /**
+     * {@inheritDoc}
+     */
+
+    public $sourcePath = '@vendor/npm-asset/jquery-ui-timepicker-addon';
+    /**
+     * {@inheritDoc}
+     */
+    public $css = [
+        'src/jquery-ui-timepicker-addon.min.css'
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public $js = [
+        'src/jquery-ui-timepicker-addon.min.js',
+        'dist/i18n/jquery-ui-timepicker-addon-i18n.min.js'
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public $depends = [
+        JuiAsset::class
+    ];
+}
